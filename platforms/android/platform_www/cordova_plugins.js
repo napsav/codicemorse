f9b322d5c4f393c4e01115a7adbcb8824a753bc4
napsav <savi.napolitano@gmail.com>
2020-07-06 00:34:32 +0200

    uploading project files

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,16 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+  module.exports = [
+    {
+      "id": "cordova-clipboard.Clipboard",
+      "file": "plugins/cordova-clipboard/www/clipboard.js",
+      "pluginId": "cordova-clipboard",
+      "clobbers": [
+        "cordova.plugins.clipboard"
+      ]
+    }
+  ];
+  module.exports.metadata = {
+    "cordova-plugin-whitelist": "1.3.4",
+    "cordova-clipboard": "1.3.0"
+  };
+});
